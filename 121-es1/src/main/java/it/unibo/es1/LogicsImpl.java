dbaccb78c34d19fdee7a94a88f0bc33001be2023
Elettra Ventura <even2728@gmail.com>
2024-12-10 16:56:56 +0000

    es 121

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,79 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
+	final List<Button> buttons;
+	final int max;
 
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.buttons = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			buttons.add(new Button());
+		}
+		this.max = size;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.buttons.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return this.buttons.stream().map(Button::getValue).collect(Collectors.toList());
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.buttons.stream().map(Button::isEnabled).collect(Collectors.toList());
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		this.buttons.get(elem).increment();
+		return this.buttons.get(elem).getValue();
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return "<<"+this.buttons.stream().map(b -> String.valueOf(b.getValue())).collect(Collectors.joining("|"))+">>";
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		Integer valueOfFirst;
+		if (!this.buttons.isEmpty()){
+			valueOfFirst = this.buttons.getFirst().getValue();
+			return this.buttons.stream().map(Button::getValue).allMatch(valueOfFirst::equals);
+		}
+		return true;
+	}
+
+	private final class Button {
+		int value;
+		boolean enabled;
+
+		Button() {
+			this.value = 0;
+			this.enabled = true;
+		}
+
+		int getValue() {
+			return this.value;
+		}
+
+		boolean isEnabled() {
+			if (this.value >= max) {
+				this.enabled = false;
+			}
+			return this.enabled;
+		}
+
+		void increment() {
+			this.value++;
+		}
 	}
 }
